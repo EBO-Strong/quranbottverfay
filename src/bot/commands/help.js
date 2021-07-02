250cd3137b7f3bd6616959c9794be2c1af6f979e
Sakran0 <85891532+Sakran0@users.noreply.github.com>
2021-07-02 23:15:18 +0300

    Update help.js

--- src/bot/commands/help.js
+++ src/bot/commands/help.js
@@ -45,26 +45,8 @@ module.exports = {
 
 **Links:**
 [ InviteBot ]() - [ ServerSupport ]()
+`)
 
-
-
-
-// .addFields(
-      //   { name: `> ${prefix}help`, value: "**`للحصول على قاءمة السماعده`**", inline: false },
-      //   { name: `> ${prefix}support`, value: "**`للحصول على روابط مساعدة البوت`**", inline: false },
-      //   { name: `> ${prefix}ping`, value: "**`للحصول على نتيجه ببينج البوت`**", inline: false },
-      //   { name: `_ _`, value: "_ _", inline: false },
-      //   { name: `> ${prefix}set-azkar-channel`, value: "**`لتحديد روم نشر الأذكار`**", inline: false },
-      //   { name: `> ${prefix}set-azkar-embed`, value: "**`للتغير ما بين نظام نشر الأذكار من رساءل عاديه ل رساءل ايمبد`**", inline: false },
-      //   { name: `> ${prefix}set-azkar-toggle`, value: "**`لايقاف و تشغيل نظام أرسال الأذكار`**", inline: false },
-      //   { name: `> ${prefix}morning`, value: "**`للحصول على اذكار الصباح`**", inline: false },
-      //   { name: `> ${prefix}evening`, value: "**`للحصول على اذكار المساء`**", inline: false },
-      //   { name: `> ${prefix}mos7f`, value: "**`للحصول على صفحات المصحف الكريم`**", inline: false },
-      //   { name: `_ _`, value: "_ _", inline: false },
-      //   { name: `> ${prefix}set-quran-channel`, value: "**`لتحديد روم اذاعة القرءان الكريم`**", inline: false },
-      //   { name: `> ${prefix}set-quran-toggle`, value: "**`لاقاف و تشغيل نظام أذاعة القرءان الكريم`**", inline: false },
-      //   { name: `> ${prefix}quran`, value: "**`لتشغيل صور معينه في القرءان الكريم`**", inline: false },
-      // )
     );
   }
 };
