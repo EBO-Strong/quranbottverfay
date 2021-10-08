5c75ecb2166ff238e1fc397f41c0689487f15508
Alinnaq1 <91326277+Alinnaq1@users.noreply.github.com>
2021-10-08 10:18:26 +0300

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -1,8 +1,8 @@
 require('dotenv').config()
 
 module.exports = {
-    token:"Nzk2MTY3MTY3MjU0NzI0NjM4.X_T-dA.wr3Za8RHTASkQE3U9BXnG0vN3Pk",
+    token:"ODYzNzUzMzQ1NjAwMzIzNTk0.YOre_Q.gJKnYGd2elfLNlsVSCjSFvebP2U",
     mongoPath:"mongodb+srv://ALEX:hvgqXetXFfBbVzb6@cluster0.epc7j.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-    prefix: "e!",
+    prefix: "a!",
     expressURL: "" // if you have a express server but it here to uptime you project
 };
